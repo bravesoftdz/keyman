b0f6cfd5abc0af3cea3be98139b4e9e86dec48d5
jahorton <jahorton@sil.org>
2020-10-20 08:46:56 +0700

    feat(common/models): dependent distribution fetch

--- common/predictive-text/worker/correction/editable-input-sequence.ts
+++ common/predictive-text/worker/correction/editable-input-sequence.ts
@@ -53,11 +53,49 @@ namespace correction {
       // to efficiently build distributions for the possible post-backspace input-sequence probabilities.
     }
 
+    // TODO:   Test this!
     getDistributionAfterSequence(priorInput: RealizedInput): Distribution<KeyedTransform> {
       // TODO:  gets the next distribution necessary to 'realize' a possible input sequence,
       // given the prior sequence to that point.  This approach is necessary, as the distributions
       // may be dependent if backspaces were received at any point.
-      return null;
+
+      // Get the keys for each selected input, then reverse the array (so pop retrieves them in LTR order).
+      let inputKeys = priorInput.map(entry => entry.sample.internalKey).reverse();
+      let nesting: DependencyTuple = null;
+      let coreIndex: number = 0;
+      let currentDistribution: Distribution<KeyedTransform>;
+
+      let key: number = null;
+      do {
+        // Get the distribution source for the current token.
+        let entry: Distribution<KeyedTransform> | DependencyTuple;
+        // If we drilled into a DependencyTuple last round, that's our source; we're dependent
+        // upon the previous Transform.
+        //
+        // On the first iteration, nesting === null; the `else` block always wins as a result.
+        // After all, there can be no dependency on a prior Transform for the first Transform sampled.
+        if(nesting) {
+          // Use prior round's key.
+          entry = nesting.dependencyMapping[key];
+        } else {
+          entry = this.coreSequence[coreIndex];
+        }
+
+        // With 3 entries in the input sequence, we return on iteration 4.
+        // We retrieve the first unsampled distribution and return that.
+        // Before the final iteration, we sample it with the previous if-else
+        // block on the next iteration.
+        if(entry instanceof DependencyTuple) {
+          nesting = entry;
+          currentDistribution = entry.baseDistribution;
+        } else {
+          nesting = null;
+          coreIndex++;
+          currentDistribution = entry;
+        }
+      } while (key = inputKeys.pop());
+
+      return currentDistribution;
     }
   }
 }
